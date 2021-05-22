9afbc491d93dcfc16709c138204d915805869c52
CalebKiragu <calebdavidbusiness@gmail.com>
2021-05-22 06:41:03 -0400

    Added moment formatted timestamp

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const prettyjson = require('prettyjson');
+
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+
+//const { generateMessage, generateLocationMessage } = require('../server/utils/message');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000
@@ -12,7 +17,30 @@ let io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log(`A new user just connected.`)
+    console.log(`A new user just connected.`);
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
+
+    socket.on('createMessage', (message, callBack) => {
+        // console.log(`createMessage:\n${prettyjson.render(message)}`);
+        console.log('createMessage:\n', message);
+
+        // io.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()            
+        // })
+
+        socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
+
+        callBack(`This is the server.`)
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.lat, coords.lng));
+    })
 
     socket.on('disconnect', () => {
         console.log(`User disconnected.`);
@@ -22,7 +50,6 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
-
 
 
 // const path = require('path');
